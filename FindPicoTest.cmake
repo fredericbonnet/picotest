79ddca6fa8e2bbd3411bca330f74f34aa5fb0662
Frédéric Bonnet <frederic.bonnet@free.fr>
2025-07-24 23:44:52 +0200

    Modernize CMake packaging

--- FindPicoTest.cmake
+++
@@ -1,9 +0,0 @@
-set(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_DIR}")
-set(PicoTest_FOUND TRUE)
-
-add_library(PicoTest INTERFACE)
-set_target_properties(PicoTest PROPERTIES
-	INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include"
-)
-
-include("${_IMPORT_PREFIX}/PicoTest.cmake")
---
+++ PicoTestConfig.cmake
@@ -0,0 +1,21 @@
+# PicoTest CMake configuration file
+include(CMakeFindDependencyMacro)
+
+# Set the path for PicoTestAddTests.cmake
+set(_PICOTEST_DISCOVER_TESTS_SCRIPT 
+    "${CMAKE_CURRENT_LIST_DIR}/PicoTestAddTests.cmake")
+
+# Include the CMake functions
+include("${CMAKE_CURRENT_LIST_DIR}/PicoTest.cmake")
+
+# Create the target
+if(NOT TARGET PicoTest::PicoTest)
+    add_library(PicoTest::PicoTest INTERFACE IMPORTED)
+    set_target_properties(PicoTest::PicoTest PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/include"
+    )
+endif()
+
+# Set standard package variables
+set(PicoTest_FOUND TRUE)
+set(PicoTest_INCLUDE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include") 
